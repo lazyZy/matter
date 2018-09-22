8fac41a29b961094534f45255946d6a98dc077ae
ZY <746234304@qq.com>
2018-09-22 22:41:59 +0800

    注释语句

---
+++ src/main/java/top/mrzhao/MatterApplication.java
@@ -0,0 +1,12 @@
+package top.mrzhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatterApplication.class, args);
+	}
+}
